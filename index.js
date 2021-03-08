c8370b19927be773aae9452545e560c584f459d6
maggiesuu <msu28@my.bcit.ca>
2021-03-07 20:21:22 -0800

    third changes index.js

--- index.js
+++ index.js
@@ -25,3 +25,17 @@ document.querySelector(".comp4_4").innerHTML += Comp4UI('Images/fridge.svg', 'Le
 
 document.querySelector(".comp5").innerHTML += Comp5UI(50,500,'Images/down-arrow.svg')
 
+
+
+
+document.querySelector(".comp9").innerHTML += Comp9UI("images/1.svg", "Have a smaller shopping list and increase the frequency of your grocery shopping trips");
+document.querySelector(".comp9").innerHTML += Comp9UI("images/2.svg", "Consume your leftover food first");
+document.querySelector(".comp9").innerHTML += Comp9UI("images/3.svg", "Make foods that can be preserved longer such as jam, sauces, and pickled food");
+document.querySelector(".comp9").innerHTML += Comp9UI("images/4.svg", "Be mindful of saving the environment by reducing food waste");
+
+document.querySelector(".comp10").innerHTML += Comp10UI("60px", "120deg", "0px", "0px", "1s");
+document.querySelector(".comp10").innerHTML += Comp10UI("75px", "80deg", "70px", "100px", "1.5s");
+document.querySelector(".comp10").innerHTML += Comp10UI("95px", "50deg", "150px", "200px", "2s");
+document.querySelector(".comp10").innerHTML += Comp10UI("115px", "80deg", "250px", "350px", "2.5s");
+document.querySelector(".comp10").innerHTML += Comp10UI("130px", "120deg", "380px", "500px", "3s");
+document.querySelector(".comp10").innerHTML += Comp10UI("145px", "150deg", "600px", "450px", "3.5s");
