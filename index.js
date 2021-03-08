9a99f1428427b05e5c7778a6ad64b2311fb65050
maggiesuu <msu28@my.bcit.ca>
2021-03-07 20:18:28 -0800

    second changes

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 document.querySelector(".comp9").innerHTML += Comp9UI("Images/1.svg", "Have a smaller shopping list and increase the frequency of your grocery shopping trips");
 document.querySelector(".comp9").innerHTML += Comp9UI("Images/2.svg", "Consume your leftover food first");
 document.querySelector(".comp9").innerHTML += Comp9UI("Images/3.svg", "Make foods that can be preserved longer such as jam, sauces, and pickled food");
@@ -9,4 +52,3 @@ document.querySelector(".comp10").innerHTML += Comp10UI("95px", "50deg", "150px"
 document.querySelector(".comp10").innerHTML += Comp10UI("115px", "80deg", "250px", "350px", "2.5s");
 document.querySelector(".comp10").innerHTML += Comp10UI("130px", "120deg", "380px", "500px", "3s");
 document.querySelector(".comp10").innerHTML += Comp10UI("145px", "150deg", "600px", "450px", "3.5s");
-
