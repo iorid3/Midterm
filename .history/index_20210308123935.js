593a5cd41135ccb5902c6e54d5df51529fc16585
ClaudiaShin <claudiashin07@gmail.com>
2021-03-08 12:44:48 +0900

    added components

--- .history/index_20210308123935.js
+++ .history/index_20210308123935.js
@@ -11,7 +11,7 @@
 //add claudia's componenets
 
 document.querySelector(".comp3_1").innerHTML += Comp3UI('Average Canadian wastes 140kg of food in a year', 'Images/crowd.svg')
-document.querySelector(".comp3_2").innerHTML += Comp3UI('That’s worth about $1,100 Canadian dollars', 'Images/plant.svg')
+document.querySelector(".comp3_2").innerHTML += Comp3UI('That’s worth about $1,100 Canadian dollars', 'Images/canada.svg')
 document.querySelector(".comp3_3").innerHTML += Comp3UI('About 63% of the food that is thrown away could be eaten or used', 'Images/garbage.svg')
 document.querySelector(".comp3_4").innerHTML += Comp3UI('A single household waste approximately $100 per month', 'Images/family.svg')
 
@@ -20,5 +20,5 @@ document.querySelector(".comp4_2").innerHTML += Comp4UI('Images/bread.svg', 'Bre
 document.querySelector(".comp4_3").innerHTML += Comp4UI('Images/fruits.svg', 'Fruits',130,380)
 document.querySelector(".comp4_4").innerHTML += Comp4UI('Images/fridge.svg', 'Leftover',130,545)
 
-document.querySelector(".comp5").innerHTML += Comp5UI(50,500,'Images/down-arrow.svg')
+document.querySelector(".comp5").innerHTML += Comp5UI(50,500,'icons/down-arrow.svg')
 
