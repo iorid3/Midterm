e0b485ecf676e6db1b1373fd61bd333a773f47d9
HenryBCIT <hleung70@bcit.ca>
2021-03-08 16:46:02 -0800

    deleted history

--- .history/index_20210307193907.js
+++
@@ -1,5 +0,0 @@
-document.querySelector(".comp1").innerHTML += Comp1UI("./Images/question.svg","25px", "What's the issue?");
-
-document.querySelector(".comp2").innerHTML += Comp2UI("./Images/professor.svg", "25px", "Good Question!");
-
-
