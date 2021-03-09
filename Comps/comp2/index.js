544c6a47d9e750829b73fd6852392fe65971e080
LeighaiNishibata <vnishibata@my.bcit.ca>
2021-03-09 01:31:22 -0800

    changes to index, css, comp 1 and 2 and js

--- Comps/comp2/index.js
+++ Comps/comp2/index.js
@@ -1,53 +1,41 @@
-function Comp2UI(url="Images/professor.svg", size ="10px", text="text"){
+function Comp2UI(padding="70px", size ="10px", text="text", url="Images/professor.svg", ){
     return `
-      <div style='
-        width: 150px;
-        height: 150px;
-        display: inline-flex;
-        justify-content: center;
-        align-items: center;
-        transition: opacity 1s;
-        opacity: 0.25;
-    '
-    onclick='Comp1UI.HandleClick(this)'
-    >
-    
-        <img style='
-            display: inline-flex;
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
-            left: 500px;
-            top:60px;
-        ' src="${url}"/>
-
         <div style='
-        width: 200px;
-        height: 200px;
+            display: inline-flex;
+            justify-content: center;
+            margin: 20px;
+            transition: opacity 1s;
+            opacity: 0.20;
+        '
+        onclick='Comp1UI.HandleClick(this)'
+        >
+        <div style='
+            width: 300px;
+            background-color: white;
+            border: 2px solid #00000;
+            border-radius: 50px;
+            padding-top:${padding};
+            text-align: center;
+            border: 2px solid black;
+            margin: 10px;
         '>
-            <img style='
-                display: inline-flex;
-                width: 100%;
-                height: 100%;
-                object-fit: cover;
-                right: 200px;
-                bottom: 10px;
-                transform: rotateY(180deg)
-            ' src="Images/chat.svg" />
 
             <div style='
-                display: inline-flex;
-                position: relative;
-                bottom: 280px;
-                right: 560px;
                 font-size: ${size};
                 font-weight: bold;
-                font-family: 'Poppins';
-            ' >${text}</div>
+            '> ${text}</div>
         </div>
-    </div>`
+
+        <img style='
+            width: 200px;
+            object-fit: cover;
+            margin: 10px;
+        'src="${url}"/>
+
+    </div>
+    `
 }
 
-Comp2UI.HandleClick = (el) => { 
+Comp1UI.HandleClick = (el) => { 
     el.style.opacity = 1;
 }
