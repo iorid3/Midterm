8ff5a2fc8de9f3a32a3a7946450b7314a14147ba
Iori Takeshita <takeshitaiori@gmail.com>
2021-03-10 15:23:59 +0900

    changes

--- Comps/comp6/index.js
+++ Comps/comp6/index.js
@@ -1,7 +1,6 @@
 function Carboncomp(text =" 7% of greenhouse gases produced globally are due to preventable food waste", url= "images/factory.svg"){
     return `<div style = "
                           display:flex;
-                          animation:shake 3s infinite;
                           ">
                          <div style = "display:flex;
                          padding:10px;">
